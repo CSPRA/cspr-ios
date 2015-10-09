a6f11584d8b0b1395d33a2b15009df29f7d60a0e
aqsa-fueled <aam@fueled.co>
2015-10-09 18:11:28 +0530

    add form for patient info

--- ICS/Controllers/PatientInformationViewController.m
+++ ICS/Controllers/PatientInformationViewController.m
@@ -7,6 +7,23 @@
 //
 
 #import "PatientInformationViewController.h"
+#import <XLForm/XLForm.h>
+
+NSString *const kFullName = @"Full Name";
+NSString *const kGender = @"Gender";
+NSString *const kDOB = @"Date of birth";
+NSString *const kPhoneNumber = @"PhoneNumber";
+NSString *const kAddPhone = @"Add Phone Number";
+NSString *const kEmail = @"Email";
+NSString *const kAddEmail = @"Add Email";
+NSString *const kAddress = @"Address";
+NSString *const kEatingHabits = @"Eating Habits";
+NSString *const kConsumeCigarettes = @"Consume Cigarettes";
+NSString *const kConsumeAlcohal = @"ConsumeAlcohal";
+NSString *const kNotes = @"notes";
+
+@interface PatientInformationViewController ()
+@end
 
 @implementation PatientInformationViewController
 
@@ -16,11 +33,139 @@
   return self;
 }
 
+- (void)didTappedButtonRow:(XLFormRowDescriptor*)sender {
+  if (sender.tag == kAddEmail) {
+    [self addRowWithTag:kEmail
+                rowType:XLFormRowDescriptorTypeEmail
+                  title:kEmail
+                section:sender.sectionDescriptor
+              beforeRow:sender];
+  }
+  if (sender.tag == kAddPhone) {
+    [self addRowWithTag:kPhoneNumber
+                rowType:XLFormRowDescriptorTypePhone
+                  title:kPhoneNumber
+                section:sender.sectionDescriptor
+              beforeRow:sender];
+    
+  }
+  
+  NSIndexPath *index = [self.tableView indexPathForSelectedRow];
+  [self.tableView deselectRowAtIndexPath:index animated:YES];
+}
+
+- (XLFormRowDescriptor*)customizationOFRow:(XLFormRowDescriptor*)row {
+  [row.cellConfigAtConfigure setObject:[UIColor clearColor] forKey:@"backgroundColor"];
+  [row.cellConfig setObject:[UIFont fontWithName:@"Helvetica" size:12] forKey:@"textLabel.font"];
+  [row.cellConfig setObject:@(NSTextAlignmentLeft) forKey:@"textLabel.textAlignment"];
+  
+  if ([row.cellConfig valueForKey:@"textField"]) {
+    
+    [row.cellConfig setObject:[UIColor lightGrayColor] forKey:@"textField.backgroundColor"];
+    [row.cellConfig setObject:[UIFont fontWithName:@"Helvetica" size:20] forKey:@"textField.font"];
+    [row.cellConfig setObject:@(NSTextAlignmentLeft) forKey:@"textField.textAlignment"];
+  }
+  
+  return row;
+}
+- (void)addRowWithTag:(NSString *)tag rowType:(NSString *)rowType title:(NSString *)title section:(XLFormSectionDescriptor*)section beforeRow:(XLFormRowDescriptor*)beforeRow{
+  XLFormRowDescriptor *row = [XLFormRowDescriptor formRowDescriptorWithTag:tag
+                                                                   rowType:rowType
+                                                                     title:title];
+  row = [self customizationOFRow:row];
+  if ([rowType isEqualToString:XLFormRowDescriptorTypeButton]) {
+    row.action.formSelector = @selector(didTappedButtonRow:);
+  }
+  
+  [section addFormRow:row beforeRow:beforeRow];
+}
+
+- (void)addRowWithTag:(NSString *)tag rowType:(NSString *)rowType title:(NSString *)title section:(XLFormSectionDescriptor*)section {
+  XLFormRowDescriptor *row = [XLFormRowDescriptor formRowDescriptorWithTag:tag
+                                                                   rowType:rowType
+                                                                     title:title];
+  row = [self customizationOFRow:row];
+  if ([rowType isEqualToString:XLFormRowDescriptorTypeButton]) {
+    row.action.formSelector = @selector(didTappedButtonRow:);
+  }
+  
+  [section addFormRow:row];
+}
 
 - (void)setupForm {
   XLFormDescriptor *form = [XLFormDescriptor formDescriptorWithTitle:@"Patient Information"];
-  XLFormSectionDescriptor *section;
-  XLFormRowDescriptor *row;
+  XLFormSectionDescriptor *section = [XLFormSectionDescriptor formSectionWithTitle:@""];
+  
+  [form addFormSection:section];
+  
+  //name field
+  [self addRowWithTag:kFullName
+              rowType:XLFormRowDescriptorTypeName
+                title:kFullName
+              section:section];
+  
+  //gender field
+  [self addRowWithTag:kGender
+              rowType:XLFormRowDescriptorTypeText
+                title:kGender
+              section:section];
+  
+  //date of birth field
+  [self addRowWithTag:kDOB
+              rowType:XLFormRowDescriptorTypeDate
+                title:kDOB
+              section:section];
+  
+  //phone number fields
+  XLFormSectionDescriptor *phoneSection = [XLFormSectionDescriptor formSectionWithTitle:@""];
+  [form addFormSection:phoneSection];
+  
+  [self addRowWithTag:kPhoneNumber
+              rowType:XLFormRowDescriptorTypePhone
+                title:kPhoneNumber
+              section:phoneSection];
+  
+  [self addRowWithTag:kAddPhone
+              rowType:XLFormRowDescriptorTypeButton
+                title:kAddPhone
+              section:phoneSection];
+  
+  //Email fields
+  XLFormSectionDescriptor *emailSection = [XLFormSectionDescriptor formSectionWithTitle:@""];
+  [form addFormSection:emailSection];
+  
+  [self addRowWithTag:kEmail
+              rowType:XLFormRowDescriptorTypeEmail
+                title:kEmail
+              section:emailSection];
+  
+  [self addRowWithTag:kAddEmail
+              rowType:XLFormRowDescriptorTypeButton
+                title:kAddEmail
+              section:emailSection];
+  
+  //address field
+  [self addRowWithTag:kAddress
+              rowType:XLFormRowDescriptorTypeTextView
+                title:kAddress
+              section:section];
+  
+  //Eating habits field
+  [self addRowWithTag:kEatingHabits
+              rowType:XLFormRowDescriptorTypeText
+                title:kEatingHabits
+              section:section];
+  
+  [self addRowWithTag:kConsumeCigarettes
+              rowType:XLFormRowDescriptorTypeBooleanSwitch
+                title:kConsumeCigarettes
+              section:section];
+  
+  [self addRowWithTag:kConsumeAlcohal
+              rowType:XLFormRowDescriptorTypeBooleanSwitch
+                title:kConsumeAlcohal
+              section:section];
+  
   
   self.form = form;
 }
